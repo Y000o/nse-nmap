e46c8587c633a6429b5aa38d1965a77d856d0d22
luismadero <46235512+luismadero@users.noreply.github.com>
2019-07-28 15:59:31 -0600

    Update nse-nmap1-2.sh

--- nse-nmap1-2.sh
+++ nse-nmap1-2.sh
@@ -3,7 +3,7 @@
 # Author: LuisMadero
 if [ "$(which nmap)" == "" ]; then
         echo -n -e "nmap need to be installed [y/N] "
-        read yorn
+        read -r yorn
         if [ "$yorn" == "y" ] || [ "$yorn" == "Y" ]; then
                 apt install nmap
         elif [ "$yorn" == "n" ] || [ "$yorn" == "N" ]; then
@@ -29,7 +29,7 @@ echo -e " [03] Acerca de mi"
 echo -e " [04] Salir"
 echo
 echo -n -e "nse-nmap > "
-read nsemenu
+read -r nsemenu
 
 if [ "$nsemenu" == "01" ] || [ "$nsemenu" == "1" ];
 then
@@ -57,134 +57,134 @@ echo -e " [10] Fingerprint de Firewall "
 echo -e " [00] Atras (menu)"
 echo
 echo -n -e "nse-nmap > "
-read nsenmap
+read -r nsenmap
 
 if [ "$nsenmap" == "01" ] || [ "$nsenmap" == "1" ];
 then
 echo
 echo -n -e "Enter (IP or Hostname): "
-read iphostname
+read -r iphostname
 echo
-nmap --script http-enum -sV $iphostname
+nmap --script http-enum -sV "$iphostname"
 read -rsp $'Press any key to continue...\n' -n 1 key
-bash $0
+bash "$0"
 
 elif [ "$nsenmap" == "02" ] || [ "$nsenmap" == "2" ];
 then
 echo
 echo -n -e "Enter (IP or Hostname): "
-read iphostname
+read -r iphostname
 echo
-nmap --script http-title -sV $iphostname
+nmap --script http-title -sV "$iphostname"
 read -rsp $'Press any key to continue...\n' -n 1 key
-bash $0
+bash "$0"
 
 elif [ "$nsenmap" == "03" ] || [ "$nsenmap" == "3" ];
 then
 echo
 echo -n -e "Enter Text File: "
-read txtfile
+read -r txtfile
 echo
-nmap --script dns-brute.nse -sV $txtfile
+nmap --script dns-brute.nse -sV "$txtfile"
 read -rsp $'Press any key to continue...\n' -n 1 key
-bash $0
+bash "$0"
 
 elif [ "$nsenmap" == "04" ] || [ "$nsenmap" == "4" ];
 then
 echo
 echo -n -e "Enter (IP or Hostname): "
-read iphostname
+read -r iphostname
 echo
-nmap -A $iphostname
+nmap -A "$iphostname"
 read -rsp $'Press any key to continue...\n' -n 1 key
-bash $0
+bash "$0"
 
 elif [ "$nsenmap" == "05" ] || [ "$nsenmap" == "5" ];
 then
 echo
 echo -n -e "Enter (IP or Hostname): "
-read iphostname
+read -r iphostname
 echo
-nmap -vv --script http-errors --script-args "httpspider.url=/docs/,httpspider.maxpagecount=3,httpspider.maxdepth=1" $iphostname
+nmap -vv --script http-errors --script-args "httpspider.url=/docs/,httpspider.maxpagecount=3,httpspider.maxdepth=1" "$iphostname"
 read -rsp $'Press any key to continue...\n' -n 1 key
-bash $0
+bash "$0"
 
 elif [ "$nsenmap" == "06" ] || [ "$nsenmap" == "6" ];
 then
 echo
 echo -n -e "Enter IPv6-Address: "
-read ipvsixaddress
+read -r ipvsixaddress
 echo
-nmap --script http-exif-spider --script-args="http.max-cache-size=99999999" -Pn $ipvsixaddress
+nmap --script http-exif-spider --script-args="http.max-cache-size=99999999" -Pn "$ipvsixaddress"
 read -rsp $'Press any key to continue...\n' -n 1 key
-bash $0
+bash "$0"
 
 elif [ "$nsenmap" == "07" ] || [ "$nsenmap" == "7" ];
 then
 echo
 echo -n -e "Enter (IP or Hostname): "
-read iphostname
+read -r iphostname
 echo
-nmap --script ftp-brute $iphostname
+nmap --script ftp-brute "$iphostname"
 read -rsp $'Press any key to continue...\n' -n 1 key
-bash $0
+bash "$0"
 
 elif [ "$nsenmap" == "08" ] || [ "$nsenmap" == "8" ];
 then
 echo
 echo -n -e "Enter (IP or Hostname): "
-read iphostname
+read -r iphostname
 echo
-nmap --script mysql-brute.nse -Pn $iphostname
+nmap --script mysql-brute.nse -Pn "$iphostname"
 echo
 read -rsp $'Press any key to continue...\n' -n 1 key
-bash $0
+bash "$0"
 
 elif [ "$nsenmap" == "09" ] || [ "$nsenmap" == "9" ];
 then
 echo
 echo -n -e "Enter (IP or Hostname): "
-read iphostname
+read -r iphostname
 echo
-nmap --script http-waf-detect --script-args="http-waf-detect.aggro,http-waf-detect.detectBodyChanges" $iphostname
+nmap --script http-waf-detect --script-args="http-waf-detect.aggro,http-waf-detect.detectBodyChanges" "$iphostname"
 read -rsp $'Press any key to continue...\n' -n 1 key
-bash $0
+bash "$0"
 
 elif [ "$nsenmap" == "10" ];
 then
 echo
 echo -n -e "Enter (IP or Hostname): "
-read iphostname
+read -r iphostname
 echo
-nmap --script http-waf-fingerprint $iphostname
+nmap --script http-waf-fingerprint "$iphostname"
 read -rsp $'Press any key to continue...\n' -n 1 key
-bash $0
+bash "$0"
 
 elif [ "$nsenmap" == "00" ] || [ "$nsenmap" == "0" ];
 then
 echo -e "\033[1;31m[!] Atras..\033[1;0m"
-bash $0
+bash "$0"
 
 else
 echo
 echo -e "\033[1;31m[!] Elegiste mal...\033[1;0m"
 sleep 1
-bash $0
+bash "$0"
 fi
 else
-	if [ "$nsemenu" == "02" ] || [ "$nsemenu" == "2" ];
+        if [ "$nsemenu" == "02" ] || [ "$nsemenu" == "2" ];
 then
 echo
 echo -e "\033[1;31m[!] Proximamente...\033[1;0m"
 sleep 1
-bash $0
+bash "$0"
 
 elif [ "$nsemenu" == "03" ] || [ "$nsemenu" == "3" ];
 then
 echo
 echo -e "\033[1;31m[!] Creado por LuisMadero... \033[1;0m"
 sleep 10
-bash $0
+bash "$0"
 elif [ "$nsemenu" == "04" ] || [ "$nsemenu" == "4" ];
 then
 exit
